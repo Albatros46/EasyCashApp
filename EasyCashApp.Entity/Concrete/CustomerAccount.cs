4e7e9af5b765f0d1a1bce8f07de316242b1bbea3
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2023-05-01 18:26:07 +0200

    2- Cash Asp.Net Core 6.0 Identity Projesi - Referans İşlemleri ve Entitylerin Yazılması

---
+++ EasyCashApp.Entity/Concrete/CustomerAccount.cs
@@ -0,0 +1,12 @@
+﻿namespace EasyCashApp.Entity.Concrete
+{
+    public class CustomerAccount
+    {
+        public int Id { get; set; }//CustomerAccoungID
+        public string CustomerAccountNumber { get; set; }
+        public string CustomerCurrency { get; set; }
+        public decimal CustomerBalance { get; set; }
+        public string BankBrach { get; set; }//Banka subesi
+
+    }
+}
---
+++ EasyCashApp.Entity/Concrete/CustomerAccountProcess.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyCashApp.Entity.Concrete
+{
+    public class CustomerAccountProcess
+    {
+        public int Id { get; set; }//CustomerAccountProcessId
+        public string ProcessType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ProcessDate { get; set; } = DateTime.UtcNow;
+    }
+}
