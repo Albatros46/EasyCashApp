4d7b3dffe624061302d5d98a4ac48de9ef2d4dea
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2023-05-02 14:36:34 +0200

    3-DbContext yazilarak veritabani balantisi olusturulup Migration islemi yapildi.

---
+++ EasyCashApp.DataAccess/Concrete/Context.cs
@@ -0,0 +1,29 @@
+﻿using EasyCashApp.Entity.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashApp.DataAccess.Concrete
+{
+    public class Context : IdentityDbContext
+    {
+       
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {// Configuration kismini appsettings.json icerisinde de yazabiliriz.
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=ALBATROS46;Database=EasyCashDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=yes;");//Kendi veritabani erisiminizi yaziniz
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountsProcess { get; set; }
+
+    }
+}
+  //"ConnectionStrings": {
+  //  "DefaultConnection": "Server=ALBATROS46;Database=SchoolProject.Web;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=yes;"
+  //}
