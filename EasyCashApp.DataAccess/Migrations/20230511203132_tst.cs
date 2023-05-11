f636a0859230ab3c061b21afa7e6b8993c4345d6
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2023-05-11 22:43:50 +0200

    7-RegisterController veritabanina kayit islemi yapildi.

---
+++ EasyCashApp.DataAccess/Migrations/20230511203132_tst.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyCashApp.DataAccess.Migrations
+{
+    public partial class tst : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- EasyCashApp.Web/Controllers/RegisterController.cs
+++ EasyCashApp.Web/Controllers/RegisterController.cs
@@ -8,18 +8,19 @@ namespace EasyCashApp.Web.Controllers
     public class RegisterController : Controller
     {
         private readonly UserManager<AppUser> _userManager;//userManager ya IDentityUser sinifi olacak yada onu miras alan bir sinif olmali
+                                                           //.Net Core Identity kütüphanesnin password kombänayonu en az 1 sayi 1 harf 1 sembol ve en az 6 karakterden olusmali
 
         public RegisterController(UserManager<AppUser> userManager)
         {
             _userManager = userManager;
         }
-
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
-
-        public async Task< IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
         {
             if(ModelState.IsValid)
             {
@@ -29,11 +30,21 @@ namespace EasyCashApp.Web.Controllers
                     FirstName = appUserRegisterDto.FirstName,
                     LastName = appUserRegisterDto.LastName,
                     Email = appUserRegisterDto.Email,
+                    City="Pforzheim",
+                    Disctrict="Test1",
+                    ImageUrl="TestImg1",
                 };
                 var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
                 if (result.Succeeded)
                 {
                     return RedirectToAction("Index","ConfirmMail");//Eger girilen mail dogru ise kontrol etmek icin 6 haneli sayi gonderip kontrol etmesini saglayacagiz
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {//Register alaninda giris yapildiginda hatayi yakalayip kullaniciya göstermek icin.
+                        ModelState.AddModelError("", item.Description);
+                    }
                 }
                
             }
