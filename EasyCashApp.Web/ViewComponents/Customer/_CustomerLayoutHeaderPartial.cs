233c84802c0ab30fc0c537e9a55f816320323152
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2023-05-28 21:09:58 +0200

    11-ViewComponent kullanilarak  index sayfasinin kod blogunu Partial seklinde seyreltme yaptik.

---
+++ EasyCashApp.Web/ViewComponents/Customer/_CustomerLayoutHeaderPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashApp.Web.ViewComponents.Customer
+{
+    public class _CustomerLayoutHeaderPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ EasyCashApp.Web/ViewComponents/Customer/_CustomerLayoutNavbarPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashApp.Web.ViewComponents.Customer
+{
+    public class _CustomerLayoutNavbarPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+   
+}
